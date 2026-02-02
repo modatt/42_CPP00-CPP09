e18641318d14384dc40c03ed2c102dee7629bec9
modatt <mariam.odat97@gmail.com>
2026-02-02 09:26:03 +0300

    updates

---
+++ CPP06/ex01/Serializer.hpp
@@ -0,0 +1,13 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+
+#include <iostream>
+
+class Serializer
+{
+    static uintptr_t serialize(Data* ptr);
+    static Data* deserialize(uintptr_t raw);
+};
+
+#endif
